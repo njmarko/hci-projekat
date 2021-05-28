e329f17b6fbf3795f3cf1c6de9577e58d78baa14
Dusan Erdeljan <dusanerdeljan99@gmail.com>
2021-05-28 18:35:47 +0200

    Added filter block on the client requests page.

--- src/HCIProjekat/UI/ViewModels/MainViewModel.cs
+++ src/HCIProjekat/UI/ViewModels/MainViewModel.cs
@@ -11,7 +11,7 @@ namespace UI.ViewModels
     {
         public MainViewModel(IApplicationContext context) : base(context)
         {
-            Context.Router.Push("Login");
+            Context.Router.Push("ClientRequests");
         }
     }
 }
