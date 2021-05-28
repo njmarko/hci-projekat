6d7f95e532f58805c74c4e3225430a2766975b02
Dusan Erdeljan <dusanerdeljan99@gmail.com>
2021-05-28 14:13:41 +0200

    Implemented client registration.

---
+++ src/HCIProjekat/UI/Commands/RegisterCommand.cs
@@ -0,0 +1,57 @@
+﻿using Domain.Entities;
+using Domain.Exceptions;
+using Domain.Services.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using UI.Context.Routers;
+using UI.ViewModels;
+
+namespace UI.Commands
+{
+    public class RegisterCommand : ICommand
+    {
+        private readonly RegisterViewModel _registerVm;
+        private readonly IClientService _clientService;
+        private readonly IRouter _router;
+
+        public event EventHandler CanExecuteChanged;
+
+        public RegisterCommand(RegisterViewModel registerVm, IClientService clientService, IRouter router)
+        {
+            _registerVm = registerVm;
+            _clientService = clientService;
+            _router = router;
+            _registerVm.PropertyChanged += _registerVm_PropertyChanged;
+        }
+
+        private void _registerVm_PropertyChanged(object sender, System.ComponentModel.PropertyChangedEventArgs e)
+        {
+            if (e.PropertyName == nameof(RegisterViewModel.CanRegister))
+            {
+                CanExecuteChanged?.Invoke(sender, e);
+            }
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return _registerVm.CanRegister;
+        }
+
+        public void Execute(object parameter)
+        {
+            try
+            { 
+                var registeredClient = _clientService.Create(new Client { FirstName = _registerVm.FirstName, LastName = _registerVm.LastName, Password = _registerVm.Password, Username = _registerVm.Username, DateOfBirth = _registerVm.DateOfBirth });
+                _router.Push("Login");
+            } 
+            catch (UsernameAlreadyExistsException exception)
+            {
+                // TODO: Error handling
+            }
+        }
+    }
+}
--- src/HCIProjekat/UI/ViewModels/RegisterViewModel.cs
+++ src/HCIProjekat/UI/ViewModels/RegisterViewModel.cs
@@ -1,16 +1,66 @@
-﻿using System;
+﻿using Domain.Services.Interfaces;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
+using UI.Commands;
 using UI.Context;
 
 namespace UI.ViewModels
 {
     public class RegisterViewModel : ViewModelBase
     {
-        public RegisterViewModel(IApplicationContext context) : base(context)
+        private string _username;
+        public string Username
         {
+            get { return _username; }
+            set { _username = value; OnPropertyChanged(nameof(Username)); OnPropertyChanged(nameof(CanRegister)); }
+        }
+
+        private string _password;
+        public string Password
+        {
+            get { return _password; }
+            set { _password = value; OnPropertyChanged(nameof(Password)); OnPropertyChanged(nameof(CanRegister)); }
+        }
+
+        private string _confirmPassword;
+        public string ConfirmPassword
+        {
+            get { return _confirmPassword; }
+            set { _confirmPassword = value; OnPropertyChanged(nameof(ConfirmPassword)); OnPropertyChanged(nameof(CanRegister)); }
+        }
+
+        private string _firstName;
+        public string FirstName
+        {
+            get { return _firstName; }
+            set { _firstName = value; OnPropertyChanged(nameof(FirstName)); OnPropertyChanged(nameof(CanRegister)); }
+        }
+
+        private string _lastName;
+        public string LastName
+        {
+            get { return _lastName; }
+            set { _lastName = value; OnPropertyChanged(nameof(LastName)); OnPropertyChanged(nameof(CanRegister)); }
+        }
+
+        private DateTime _dateOfBirth = DateTime.Now;
+        public DateTime DateOfBirth
+        {
+            get { return _dateOfBirth; }
+            set { _dateOfBirth = value; OnPropertyChanged(nameof(DateOfBirth)); OnPropertyChanged(nameof(CanRegister)); }
+        }
+
+        public bool CanRegister =>
+            !string.IsNullOrEmpty(FirstName) && !string.IsNullOrEmpty(LastName) && !string.IsNullOrEmpty(Username) && !string.IsNullOrEmpty(Password) && (Password == ConfirmPassword);
+        public ICommand RegisterCommand { get; private set; }
+
+        public RegisterViewModel(IApplicationContext context, IClientService clientService) : base(context)
+        {
+            RegisterCommand = new RegisterCommand(this, clientService, context.Router);
         }
     }
 }
