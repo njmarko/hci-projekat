515885524974733ea0aacde06a1d7ef351dc1dd8
njmarko <marko.njegomir@uns.ac.rs>
2021-05-28 22:32:58 +0200

    Added location when creating partner.

--- src/HCIProjekat/UI/Commands/RegisterPartnerCommand.cs
+++ src/HCIProjekat/UI/Commands/RegisterPartnerCommand.cs
@@ -46,7 +46,12 @@ namespace UI.Commands
         {
             try
             {
-                var registerPartner = _partnerService.Create(new Partner { Name = _registerVm.Name, Type = _registerVm.Type, Location = _registerVm.Location});
+                var registerPartner = _partnerService.Create(new Partner
+                {
+                    Name = _registerVm.Name,
+                    Type = _registerVm.Type,
+                    Location = new Location { City = _registerVm.City, Country = _registerVm.Country, Street = _registerVm.Street, StreetNumber = _registerVm.StreetNumber }
+                });
                 // Just a message to show it works. Success message will be changed after the windows are connected it also redirects to aprtners for now. This should be changed
                 // TODO: Change message displaying for successfull registration
                 MessageBox.Show($"Partner sucessfuly added, {registerPartner.Name}.");
