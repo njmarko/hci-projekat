0255b9bd84b631b403d25a9b78d4be1caea7e3d6
Dusan Erdeljan <dusanerdeljan99@gmail.com>
2021-05-29 19:57:17 +0200

    Implemented redirection from navbar.

--- src/HCIProjekat/UI/ViewModels/NavbarViewModel.cs
+++ src/HCIProjekat/UI/ViewModels/NavbarViewModel.cs
@@ -5,6 +5,7 @@ using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 using UI.Context;
 
 namespace UI.ViewModels
@@ -13,6 +14,8 @@ namespace UI.ViewModels
     {
         public string Name { get; set; }
         public string Route { get; set; }
+        public bool IsSelected { get; set; }
+        public ICommand RouterPushCommand { get; set; }
     }
 
     public class NavbarViewModel : ViewModelBase
@@ -39,15 +42,19 @@ namespace UI.ViewModels
                 User user = Context.Store.CurrentUser;
                 if (user is Admin)
                 {
-                    NavbarItems.Add(new NavbarItemModel { Name = "Admin", Route = "" });
+                    NavbarItems.Add(new NavbarItemModel { Name = "Partners", Route = "AdminPartners", IsSelected = currentVm is AdminPartnersViewModel, RouterPushCommand = Context.Router.RouterPushCommand });
+                    NavbarItems.Add(new NavbarItemModel { Name = "Event planners", Route = "AdminEventPlanners", IsSelected = currentVm is AdminEventPlannersViewModel, RouterPushCommand = Context.Router.RouterPushCommand });
+                    NavbarItems.Add(new NavbarItemModel { Name = "Clients", Route = "AdminClients", IsSelected = currentVm is AdminClientsViewModel, RouterPushCommand = Context.Router.RouterPushCommand });
                 } 
                 else if (user is EventPlanner)
                 {
-                    NavbarItems.Add(new NavbarItemModel { Name = "Event planner", Route = "" });
+                    NavbarItems.Add(new NavbarItemModel { Name = "Home", Route = "EventPlannerHome", IsSelected = currentVm is EventPlannerHomeViewModel, RouterPushCommand = Context.Router.RouterPushCommand });
+                    NavbarItems.Add(new NavbarItemModel { Name = "Partners", Route = "EventPlannerHome", IsSelected = false, RouterPushCommand = Context.Router.RouterPushCommand });
+                    NavbarItems.Add(new NavbarItemModel { Name = "Requests", Route = "EventPlannerHome", IsSelected = false, RouterPushCommand = Context.Router.RouterPushCommand });
                 }
                 else
                 {
-                    NavbarItems.Add(new NavbarItemModel { Name = "User", Route = "" });
+                    NavbarItems.Add(new NavbarItemModel { Name = "Home", Route = "ClientRequests", IsSelected = currentVm is ClientRequestsViewModel, RouterPushCommand = Context.Router.RouterPushCommand });
                 }
             }
         }
