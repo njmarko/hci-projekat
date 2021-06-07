46c6e56d829b23580ddaa95b64754f01c66818ac
Dusan Erdeljan <dusanerdeljan99@gmail.com>
2021-06-07 20:27:17 +0200

    Seating layout domain setup.

---
+++ src/HCIProjekat/Domain/Entities/ILayoutItem.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    public interface ILayoutItem
+    {
+        public string Label { get; set; }
+        public double X { get; set; }
+        public double Y { get; set; }
+        public double Radius { get; set; }
+    }
+}
---
+++ src/HCIProjekat/Domain/Entities/SeatingLayout.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    public class SeatingLayout : BaseEntity
+    {
+        public int OfferId { get; set; }
+
+        public virtual Offer Offer { get; set; }
+        public virtual List<Table> Tables { get; set; }
+    }
+}
