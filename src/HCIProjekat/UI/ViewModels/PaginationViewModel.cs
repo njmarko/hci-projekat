41df6cf3dc9d4168f274021dcd448afd3490b143
Dusan Erdeljan <dusanerdeljan99@gmail.com>
2021-06-09 12:16:10 +0200

    Added pagination tooltips.

--- src/HCIProjekat/UI/ViewModels/PaginationViewModel.cs
+++ src/HCIProjekat/UI/ViewModels/PaginationViewModel.cs
@@ -13,6 +13,7 @@ namespace UI.ViewModels
         public int Page { get; set; }
         public bool IsChecked { get; set; } = false;
         public bool IsEnabled { get; set; } = true;
+        public string ToolTip { get; set; }
     }
 
     public class PaginationViewModel
@@ -31,12 +32,12 @@ namespace UI.ViewModels
             PerPage = perPage;
             PageCount = pageCount;
 
-            PageModels.Add(new PageModel { Text = "<<", Page = 0, IsEnabled = Page != 0 });
-            PageModels.Add(new PageModel { Text = $"<", Page = Page - 1, IsEnabled = Page != 0 });
+            PageModels.Add(new PageModel { Text = "<<", Page = 0, IsEnabled = Page != 0, ToolTip = "Go to the first page" });
+            PageModels.Add(new PageModel { Text = $"<", Page = Page - 1, IsEnabled = Page != 0, ToolTip = "Go to the page before the current" });
             var currentPage = PageCount == 0 ? 0 : Page + 1;
-            PageModels.Add(new PageModel { Text = $"{currentPage} / {PageCount}", Page = page, IsChecked = true } );
-            PageModels.Add(new PageModel { Text = $">", Page = Page + 1, IsEnabled = Page != PageCount - 1 });
-            PageModels.Add(new PageModel { Text = ">>", Page = -1, IsEnabled = Page != PageCount - 1 });
+            PageModels.Add(new PageModel { Text = $"{currentPage} / {PageCount}", Page = page, IsChecked = true, ToolTip = "Current page" } );
+            PageModels.Add(new PageModel { Text = $">", Page = Page + 1, IsEnabled = Page != PageCount - 1, ToolTip = "Go to the page after the current" });
+            PageModels.Add(new PageModel { Text = ">>", Page = -1, IsEnabled = Page != PageCount - 1, ToolTip = "Go to the last page" });
         }
     }
 }
